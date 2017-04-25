2791dc1b3e5ef9a717925f7f8ebb144ab4992664
Bill Hinostroza <billh93@gmail.com>
2017-04-25 17:58:14 -0500

    refactored: Took down the previous solution for question1 to make use of constant space.

--- question_1.py
+++ question_1.py
@@ -1,37 +1,40 @@
 """
 Explanation
 
-We can check whether two strings are anagram by comparing the characters from both strings and
-if length of both strings are the same, then the two strings are anagram. We can first initialize a list
-to store the original string in that list and then check with every possible consecutive substring in s.
-If any set is an anagram of s, then we return True, else False.
+We can check whether two strings are a possible anagram by comparing the characters from both strings
+We do this by converting them into a list and then sorting them and comparing them to each other
+We then run a for loop to compare if the anagram is truly an anagram by reiterating
+every character in the list and compare it to the other list. By using the amount of space
+with the algorithm it's uses is independent of the input parameters thus making use
+of constant space.
 
-Worst case is O(len(s)
+Worst case is O(len(s))
 Time complexity is O(len(s))
 Space complexity is O(1).
 """
 
 
+def is_anagram(s1, s2):
+    # Converts s1 and s2 arguments into lists
+    s1 = list(s1)
+    s2 = list(s2)
+    # Sorts both arguments alphabetically
+    s1.sort()
+    s2.sort()
+    # Checks if both arguments are the same and returns true or false
+    return s1 == s2
+
+
 def question1(s, t):
-    # If substring is longer than the word return false
-    if len(t) > len(s):
-        return False
-    # Initializes a list to hold the characters
-    char_list = []
-    # Iterates through every character of the string
-    for char_s in s:
-        # Adds the character into our newly created char_list
-        char_list.append(char_s)
-    #  Iterates through every character of the substring
-    for char1 in t:
-        found = False
-        for index, char2 in enumerate(char_list):
-            # Compares the character from char_list to the substring
-            if char1 == char2:
-                found = True
-        if not found:
-            return False
-    return True
+    # Gets length of substring
+    match_length = len(t)
+    # Gets length of word
+    pattern_length = len(s)
+    # Checks to see if it's an anagram by running a for loop
+    for i in range(pattern_length - match_length + 1):
+        if is_anagram(s[i: i+match_length], t):
+            return True
+    return False
 
 
 """
